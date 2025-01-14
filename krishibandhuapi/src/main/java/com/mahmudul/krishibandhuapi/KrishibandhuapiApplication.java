4354c1c4fcd212da0c9d515c588314a3c973c1cb
Hasa0891 <mshsohel@gmail.com>
2025-01-14 11:42:00 +0600

    KrishiBandhuAPI :: Initialized with java SpringBoot

---
+++ krishibandhuapi/src/main/java/com/mahmudul/krishibandhuapi/KrishibandhuapiApplication.java
@@ -0,0 +1,13 @@
+package com.mahmudul.krishibandhuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrishibandhuapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrishibandhuapiApplication.class, args);
+	}
+
+}
---
+++ krishibandhuapi/src/test/java/com/mahmudul/krishibandhuapi/KrishibandhuapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmudul.krishibandhuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrishibandhuapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
