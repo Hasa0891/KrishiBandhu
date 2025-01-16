9865b4b0dbcd6de013141cb2c550b618b37c66bb
Hasa0891 <mshsohel@gmail.com>
2025-01-16 23:02:21 +0600

    KrishiBandhuAPI :: Crops info and Crops price controller added

---
+++ krishibandhuapi/src/main/java/com/mahmudul/krishibandhuapi/crops/info/CropsInfoController.java
@@ -0,0 +1,9 @@
+package com.mahmudul.krishibandhuapi.crops.info;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/crops/info")
+public class CropsInfoController {
+}
---
+++ krishibandhuapi/src/main/java/com/mahmudul/krishibandhuapi/crops/price/CropsPriceController.java
@@ -0,0 +1,9 @@
+package com.mahmudul.krishibandhuapi.crops.price;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/crops/price")
+public class CropsPriceController {
+}
