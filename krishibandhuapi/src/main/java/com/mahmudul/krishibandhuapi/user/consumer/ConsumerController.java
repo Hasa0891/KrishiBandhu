398ef15931b1b5df1af6154f75f1f1b5a4b0ae3e
Hasa0891 <mshsohel@gmail.com>
2025-01-20 16:05:10 +0600

    KrishiBandhuAPI :: User, Admin, Farmer schema changed

--- krishibandhuapi/src/main/java/com/mahmudul/krishibandhuapi/user/consumer/ConsumerController.java
+++
@@ -1,9 +0,0 @@
-package com.mahmudul.krishibandhuapi.user.consumer;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/user/consumer")
-public class ConsumerController {
-}
--- krishibandhuapi/src/main/java/com/mahmudul/krishibandhuapi/user/supplier/SupplierController.java
+++
@@ -1,9 +0,0 @@
-package com.mahmudul.krishibandhuapi.user.supplier;
-
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RestController
-@RequestMapping("/user/supplier")
-public class SupplierController {
-}
